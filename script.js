93eb8fb1e99151ee6e905e47ca83bf053510d1bb
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2020-05-13 09:17:00 +0300

    2nd Stage

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let obj = {
-    name: 'Vlad',
-    isAdmin: true,
-}
+const menu = document.querySelector('.menu'),
+      wrapper = document.querySelector('.wrapper'),
+      aside = document.querySelector('.aside');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('menu_active');
+  wrapper.classList.toggle('wrapper_innactive');
+  aside.classList.toggle('aside_active');
+});
